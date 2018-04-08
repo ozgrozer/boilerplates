555f6a188c982a221eb65655e602c3fa33a2623d
Ozgur <ozgr@live.com>
2018-04-08 16:22:18 +0300

    Upgrade react-parcel dependencies.

--- react-parcel/src/index.jsx
+++ react-parcel/src/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const App = () => {
-  return (
-    <h1>Hello World!</h1>
-  )
+class App extends React.Component {
+  render () {
+    return (
+      <h1>Hello World!</h1>
+    )
+  }
 }
 
 ReactDOM.render(<App />, document.getElementById('root'))
