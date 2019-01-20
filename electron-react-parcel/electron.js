6adcc4158e73b61f9292f0b350eeed5a1eaac53b
ozgrozer <ozgr@live.com>
2019-01-20 15:27:29 +0300

    Upgrade packages and fix problems.

--- electron-react-parcel/electron.js
+++ electron-react-parcel/electron.js
@@ -3,8 +3,11 @@ const isDev = require('electron-is-dev')
 let win
 
 const createWindow = () => {
-  win = new BrowserWindow({ width: 640, height: 480 })
+  win = new BrowserWindow({ width: 640, height: 480, show: false })
   win.loadURL(isDev ? 'http://localhost:1234' : `file://${__dirname}/public/index.html`)
+  win.once('ready-to-show', () => {
+    win.show()
+  })
   win.on('closed', () => {
     win = null
   })
@@ -12,12 +15,12 @@ const createWindow = () => {
 
 app.on('ready', createWindow)
 
-app.on('window-all-closed', () => {
-  app.quit()
-})
-
 app.on('activate', () => {
   if (win === null) {
     createWindow()
   }
+})
+
+app.on('window-all-closed', () => {
+  app.quit()
 })
