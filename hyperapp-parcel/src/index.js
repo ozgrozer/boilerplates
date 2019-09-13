0f5c4bc80ac2f935599f4a118402e33fd7a536d2
Ozgur Ozer <ozgr@live.com>
2019-09-13 18:17:43 +0300

    Upgrade packages.

--- hyperapp-parcel/src/index.js
+++ hyperapp-parcel/src/index.js
@@ -1,20 +1,12 @@
 import { h, app } from 'hyperapp'
 
-const state = {
-  count: 0
-}
-
-const actions = {
-  down: value => state => ({ count: state.count - value }),
-  up: value => state => ({ count: state.count + value })
-}
-
-const view = (state, actions) => (
-  <div>
-    <h1>{state.count}</h1>
-    <button onclick={() => actions.down(1)}>-</button>
-    <button onclick={() => actions.up(1)}>+</button>
-  </div>
-)
-
-app(state, actions, view, document.body)
+app({
+  init: 0,
+  view: state =>
+    h('div', {}, [
+      h('h1', {}, state),
+      h('button', { onClick: state => state - 1 }, '-'),
+      h('button', { onClick: state => state + 1 }, '+')
+    ]),
+  node: document.getElementById('root')
+})
