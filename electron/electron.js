6a2fdc009bad96fe5eb57757002da5124cb66761
Ozgur Ozer <ozgr@live.com>
2020-12-11 00:22:41 +0300

    Upgrade packages

--- electron/electron.js
+++ electron/electron.js
@@ -1,25 +1,22 @@
 const { app, BrowserWindow } = require('electron')
-let win
 
 const createWindow = () => {
-  win = new BrowserWindow({ width: 640, height: 480, show: false })
-  win.loadURL(`file://${__dirname}/src/index.html`)
-  win.once('ready-to-show', () => {
-    win.show()
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600
   })
-  win.on('closed', () => {
-    win = null
-  })
+
+  mainWindow.loadFile('./src/index.html')
 }
 
-app.on('ready', createWindow)
+app.whenReady().then(() => {
+  createWindow()
 
-app.on('window-all-closed', () => {
-  app.quit()
+  app.on('activate', function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
 })
 
-app.on('activate', () => {
-  if (win === null) {
-    createWindow()
-  }
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit()
 })
