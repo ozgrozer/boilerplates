d64c9b8b7fda2de476b260143ed1608b8a255687
Ozgur <ozgr@live.com>
2022-01-13 18:42:27 +0300

    Upgrade next library

--- next/pages/index.jsx
+++ next/pages/index.jsx
@@ -1,18 +1,13 @@
-/* eslint react/jsx-fragments: 0 */
-
-import React from 'react'
 import Head from 'next/head'
 
-const Index = () => {
+export default () => {
   return (
-    <React.Fragment>
+    <>
       <Head>
         <title>Next App</title>
       </Head>
 
       <div>Hello World!</div>
-    </React.Fragment>
+    </>
   )
 }
-
-export default Index
