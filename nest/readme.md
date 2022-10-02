303603f85b353c325cfcc4095e4cf84a2aef448b
Ozgur <ozgr@live.com>
2022-10-02 18:23:59 +0300

    Add Nest.js

---
+++ nest/readme.md
@@ -0,0 +1,19 @@
+# nest
+
+Install dependencies.
+
+```
+yarn install
+```
+
+Start development.
+
+```
+yarn start:dev
+```
+
+Build the app.
+
+```
+yarn build
+```
---
+++ nest/src/app.controller.spec.ts
@@ -0,0 +1,22 @@
+import { AppService } from './app.service'
+import { AppController } from './app.controller'
+import { Test, TestingModule } from '@nestjs/testing'
+
+describe('AppController', () => {
+  let appController: AppController
+
+  beforeEach(async () => {
+    const app: TestingModule = await Test.createTestingModule({
+      controllers: [AppController],
+      providers: [AppService],
+    }).compile()
+
+    appController = app.get<AppController>(AppController)
+  })
+
+  describe('root', () => {
+    it('should return "Hello World!"', () => {
+      expect(appController.getHello()).toBe('Hello World!')
+    })
+  })
+})
