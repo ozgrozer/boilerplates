a157f08fa7cd1d7e28972b8a1b56afc2e578cf18
Ozgur Ozer <ozgr@live.com>
2024-12-25 22:13:45 +0300

    Update to app router

---
+++ next/app/layout.jsx
@@ -0,0 +1,9 @@
+export default ({ children }) => {
+  return (
+    <html lang='en'>
+      <body>
+        {children}
+      </body>
+    </html>
+  )
+}
---
+++ next/app/page.jsx
@@ -0,0 +1,9 @@
+export const metadata = {
+  title: 'Next App'
+}
+
+export default () => {
+  return (
+    <div>Hello World!</div>
+  )
+}
