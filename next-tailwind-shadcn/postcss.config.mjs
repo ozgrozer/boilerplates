e06affee25443e44c77a496b753c93b7b0ffc216
Ozgur Ozer <ozgr@live.com>
2025-03-10 22:13:05 +0300

    Update versions

--- next-tailwind-shadcn/postcss.config.mjs
+++ next-tailwind-shadcn/postcss.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('postcss-load-config').Config} */
 export default {
   plugins: {
-    tailwindcss: {}
+    'tailwindcss/nesting': {},
+    tailwindcss: {},
+    autoprefixer: {}
   }
 }
