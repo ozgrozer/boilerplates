05eba6984b333e596684bb11c08ce059ad2f0f56
Ozgur Ozer <ozgr@live.com>
2025-03-30 14:11:01 +0300

    Add button

--- next-tailwind-shadcn-ts/app/page.tsx
+++ next-tailwind-shadcn-ts/app/page.tsx
@@ -1,7 +1,23 @@
+'use client'
+
+import { useState } from 'react'
+
+import { Button } from '@/components/ui/button'
+
 export default function Home () {
+  const [count, setCount] = useState(0)
+
   return (
-    <div className='flex items-center justify-center min-h-screen'>
-      Hello world!
+    <div className='flex items-center justify-center min-h-screen flex-col gap-4'>
+      Hello world! {count}
+      <div className='flex gap-2'>
+        <Button variant='outline' onClick={() => setCount(prev => prev - 1)}>
+          Decrease
+        </Button>
+        <Button variant='outline' onClick={() => setCount(prev => prev + 1)}>
+          Increase
+        </Button>
+      </div>
     </div>
   )
 }
